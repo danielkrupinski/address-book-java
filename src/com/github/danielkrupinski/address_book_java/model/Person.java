9bd025878b73ce4b083a106bfd37dec05e955a67
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-28 20:01:17 +0200

    Add class Person with some private final fields

--- src/com/github/danielkrupinski/address_book_java/model/Person.java
+++ src/com/github/danielkrupinski/address_book_java/model/Person.java
@@ -5,3 +5,12 @@ import java.time.LocalDate;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty; import javafx.beans.property.SimpleObjectProperty; import javafx.beans.property.SimpleStringProperty; import javafx.beans.property.StringProperty;
+
+public class Person {
+    private final StringProperty firstName;
+    private final StringProperty lastName;
+    private final StringProperty street;
+    private final IntegerProperty postalCode;
+    private final StringProperty city;
+    private final ObjectProperty<LocalDate> birthday;
+}
