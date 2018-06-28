31ea5f4e67f7e106de1a764ca15392d605f131fb
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-28 19:51:16 +0200

    Add imports

--- src/com/github/danielkrupinski/address_book_java/model/Person.java
+++ src/com/github/danielkrupinski/address_book_java/model/Person.java
@@ -1,1 +1,7 @@
 package com.github.danielkrupinski.address_book_java.model;
+
+import java.time.LocalDate;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty; import javafx.beans.property.SimpleObjectProperty; import javafx.beans.property.SimpleStringProperty; import javafx.beans.property.StringProperty;
