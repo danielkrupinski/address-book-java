ce9928414170c2cd3f02776a2950a60cdaa6ee23
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-28 20:39:06 +0200

    Update formatting

--- src/com/github/danielkrupinski/address_book_java/model/Person.java
+++ src/com/github/danielkrupinski/address_book_java/model/Person.java
@@ -4,7 +4,10 @@ import java.time.LocalDate;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleIntegerProperty; import javafx.beans.property.SimpleObjectProperty; import javafx.beans.property.SimpleStringProperty; import javafx.beans.property.StringProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class Person {
     private final StringProperty firstName;
