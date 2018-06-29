d2c7118e360425479469d12e031acffdbc3828ec
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-29 13:46:29 +0200

    Import MainApp and Person classes

--- src/com/github/danielkrupinski/address_book_java/view/PersonOverviewController.java
+++ src/com/github/danielkrupinski/address_book_java/view/PersonOverviewController.java
@@ -4,3 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+
+import com.github.danielkrupinski.address_book_java.MainApp;
+import com.github.danielkrupinski.address_book_java.model.Person;
