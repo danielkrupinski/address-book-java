0629e11d4340ec5b1e112b9c1fde00238f11d5b3
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-29 13:45:28 +0200

    Import standard classes

--- src/com/github/danielkrupinski/address_book_java/view/PersonOverviewController.java
+++ src/com/github/danielkrupinski/address_book_java/view/PersonOverviewController.java
@@ -1,1 +1,6 @@
 package com.github.danielkrupinski.address_book_java.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
