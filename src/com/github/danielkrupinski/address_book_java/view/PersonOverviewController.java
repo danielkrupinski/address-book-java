06e70e0469c68a8b50600ee74007c725d259450f
Daniel Krupiński <danielkrupinski@outlook.com>
2018-06-29 13:49:08 +0200

    Add PersonOverviewController public class

--- src/com/github/danielkrupinski/address_book_java/view/PersonOverviewController.java
+++ src/com/github/danielkrupinski/address_book_java/view/PersonOverviewController.java
@@ -7,3 +7,45 @@ import javafx.scene.control.TableView;
 
 import com.github.danielkrupinski.address_book_java.MainApp;
 import com.github.danielkrupinski.address_book_java.model.Person;
+
+public class PersonOverviewController {
+    @FXML
+    private TableView<Person> personTable;
+    @FXML
+    private TableColumn<Person, String> firstNameColumn;
+    @FXML
+    private TableColumn<Person, String> lastNameColumn;
+
+    @FXML
+    private Label firstNameLabel;
+    @FXML
+    private Label lastNameLabel;
+    @FXML
+    private Label streetLabel;
+    @FXML
+    private Label postalCodeLabel;
+    @FXML
+    private Label cityLabel;
+    @FXML
+    private Label birthdayLabel;
+
+    private MainApp mainApp;
+
+    public PersonOverviewController()
+    {
+
+    }
+
+    @FXML
+    private void initialize()
+    {
+        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+    }
+
+    public void setMainApp(MainApp mainApp)
+    {
+        this.mainApp = mainApp;
+        personTable.setItems(mainApp.getPersonData());
+    }
+}
